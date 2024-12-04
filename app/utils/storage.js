827cacf4da1fb5a326661365acf322f269061032
Miguel Silva <miguel.silva@hvdgroup.com>
2024-12-04 19:44:42 +0000

    feat: storage logic

--- app/utils/storage.js
+++ app/utils/storage.js
@@ -1,1 +1,18 @@
 //initial logic for storage handle
+export const storageHandler = {
+    setItem(key, value) {
+        localStorage
+            .setItem(key, JSON.stringify(value));
+    },
+    getItem(key) {
+        return JSON.parse(localStorage
+            .getItem(key));
+    },
+    removeItem(key) {
+        localStorage
+            .removeItem(key);
+    },
+    clear() {
+        localStorage.clear();
+    }
+}
